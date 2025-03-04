5625cd96b05447b31619b00b72463b8771c9c628
darylduck <daryl@lyraddigital.com>
2025-03-05 03:17:55 +1100

    enum tests for communication service began

---
+++ lambda/packages/communication/src/events/communication-action-types/communication-action-types.spec.ts
@@ -0,0 +1,17 @@
+import { CommunicationActionTypes } from "./communication-action-types";
+
+describe("CommunicationActionTypes tests", () => {
+  it("correct string set for each enum value", () => {
+    // Arrange / Action / Assert
+    expect(CommunicationActionTypes.userJoined).toBe("userJoined");
+    expect(CommunicationActionTypes.userLeft).toBe("userLeft");
+    expect(CommunicationActionTypes.roomRemoved).toBe("roomRemoved");
+    expect(CommunicationActionTypes.changeHost).toBe("changeHost");
+    expect(CommunicationActionTypes.gameInitialized).toBe("gameInitialized");
+    expect(CommunicationActionTypes.gameStarted).toBe("gameStarted");
+    expect(CommunicationActionTypes.gameCompleted).toBe("gameCompleted");
+    expect(CommunicationActionTypes.disableGameStart).toBe("disableGameStart");
+    expect(CommunicationActionTypes.enableGameStart).toBe("enableGameStart");
+    expect(CommunicationActionTypes.hostTransfer).toBe("hostTransfer");
+  });
+});
