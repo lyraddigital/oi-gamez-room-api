0f72d231b723764d843df98b4d469946c089be87
darylduck <daryl@lyraddigital.com>
2025-03-05 03:03:07 +1100

    Finished communication service unit tests

--- lambda/packages/communication/src/communication-service/communication-service.spec.ts
+++ lambda/packages/communication/src/communication-service/communication-service.spec.ts
@@ -1,7 +1,8 @@
 import { PostToConnectionCommand } from "@aws-sdk/client-apigatewaymanagementapi";
+import { DeleteConnectionCommand } from "@aws-sdk/client-eventbridge";
 import { RoomConnection } from "@oigamez/models";
 
-describe("broadcast tests", () => {
+describe("communication service tests", () => {
   let sendFn = jest.fn();
 
   beforeEach(() => {
@@ -15,93 +16,147 @@ describe("broadcast tests", () => {
     });
   });
 
-  it("one of the connections raises an error, error ignored and all other connections are broadcasted", async () => {
-    // Arrange
-    const connections: RoomConnection[] = [
-      { connectionId: "conn123" } as RoomConnection,
-      { connectionId: "conn456" } as RoomConnection,
-      { connectionId: "conn789" } as RoomConnection,
-    ];
-    const payload = { prop: "value" };
+  describe("broadcast tests", () => {
+    it("one of the connections raises an error, error logged and all other connections are broadcasted", async () => {
+      // Arrange
+      const connections: RoomConnection[] = [
+        { connectionId: "conn123" } as RoomConnection,
+        { connectionId: "conn456" } as RoomConnection,
+        { connectionId: "conn789" } as RoomConnection,
+      ];
+      const payload = { prop: "value" };
 
-    sendFn
-      .mockImplementationOnce(() => Promise.resolve())
-      .mockImplementationOnce(() => Promise.reject())
-      .mockImplementationOnce(() => Promise.resolve());
+      sendFn
+        .mockImplementationOnce(() => Promise.resolve())
+        .mockImplementationOnce(() => Promise.reject())
+        .mockImplementationOnce(() => Promise.resolve());
 
-    const { broadcast } = await import("./communication-service");
+      const { broadcast } = await import("./communication-service");
 
-    // Action / Assert
-    expect(async () => await broadcast(connections, payload)).not.toThrow();
+      // Action / Assert
+      expect(async () => await broadcast(connections, payload)).not.toThrow();
 
-    expect(sendFn.mock.calls.length).toBe(3);
-    expect(
-      (sendFn.mock.calls[0][0] as PostToConnectionCommand).input.ConnectionId
-    ).toBe(connections[0].connectionId);
-    expect(
-      (sendFn.mock.calls[0][0] as PostToConnectionCommand).input.Data
-    ).toEqual(JSON.stringify(payload));
-    expect(
-      (sendFn.mock.calls[1][0] as PostToConnectionCommand).input.ConnectionId
-    ).toBe(connections[1].connectionId);
-    expect(
-      (sendFn.mock.calls[1][0] as PostToConnectionCommand).input.Data
-    ).toEqual(JSON.stringify(payload));
-    expect(
-      (sendFn.mock.calls[2][0] as PostToConnectionCommand).input.ConnectionId
-    ).toBe(connections[2].connectionId);
-    expect(
-      (sendFn.mock.calls[2][0] as PostToConnectionCommand).input.Data
-    ).toEqual(JSON.stringify(payload));
+      expect(sendFn.mock.calls.length).toBe(3);
+      expect(
+        (sendFn.mock.calls[0][0] as PostToConnectionCommand).input.ConnectionId
+      ).toBe(connections[0].connectionId);
+      expect(
+        (sendFn.mock.calls[0][0] as PostToConnectionCommand).input.Data
+      ).toEqual(JSON.stringify(payload));
+      expect(
+        (sendFn.mock.calls[1][0] as PostToConnectionCommand).input.ConnectionId
+      ).toBe(connections[1].connectionId);
+      expect(
+        (sendFn.mock.calls[1][0] as PostToConnectionCommand).input.Data
+      ).toEqual(JSON.stringify(payload));
+      expect(
+        (sendFn.mock.calls[2][0] as PostToConnectionCommand).input.ConnectionId
+      ).toBe(connections[2].connectionId);
+      expect(
+        (sendFn.mock.calls[2][0] as PostToConnectionCommand).input.Data
+      ).toEqual(JSON.stringify(payload));
+    });
+
+    it("one of the connections is missing a connectionId, does not process that connection", async () => {
+      // Arrange
+      const connections: RoomConnection[] = [
+        { connectionId: "conn123" } as RoomConnection,
+        {} as RoomConnection,
+        { connectionId: "conn789" } as RoomConnection,
+      ];
+      const payload = { prop: "value" };
+
+      sendFn
+        .mockImplementationOnce(() => Promise.resolve())
+        .mockImplementationOnce(() => Promise.resolve())
+        .mockImplementationOnce(() => Promise.resolve());
+
+      const { broadcast } = await import("./communication-service");
+
+      // Action
+      await broadcast(connections, payload);
+
+      expect(sendFn.mock.calls.length).toBe(2);
+      expect(
+        (sendFn.mock.calls[0][0] as PostToConnectionCommand).input.ConnectionId
+      ).toBe(connections[0].connectionId);
+      expect(
+        (sendFn.mock.calls[1][0] as PostToConnectionCommand).input.ConnectionId
+      ).toBe(connections[2].connectionId);
+    });
+
+    it("payload is undefined, does not process any connections", async () => {
+      // Arrange
+      const connections: RoomConnection[] = [
+        { connectionId: "conn123" } as RoomConnection,
+        { connectionId: "conn456" } as RoomConnection,
+        { connectionId: "conn789" } as RoomConnection,
+      ];
+
+      sendFn
+        .mockImplementationOnce(() => Promise.resolve())
+        .mockImplementationOnce(() => Promise.resolve())
+        .mockImplementationOnce(() => Promise.resolve());
+
+      const { broadcast } = await import("./communication-service");
+
+      // Action
+      await broadcast(connections, undefined);
+
+      // Assert
+      expect(sendFn.mock.calls.length).toBe(0);
+    });
   });
 
-  it("one of the connections is missing a connectionId, does not process that connection", async () => {
-    // Arrange
-    const connections: RoomConnection[] = [
-      { connectionId: "conn123" } as RoomConnection,
-      {} as RoomConnection,
-      { connectionId: "conn789" } as RoomConnection,
-    ];
-    const payload = { prop: "value" };
+  describe("closeConnection tests", () => {
+    it("connection id is not set, does not attempt to close the connection", async () => {
+      // Arrange
+      sendFn.mockImplementationOnce(() => Promise.resolve());
 
-    sendFn
-      .mockImplementationOnce(() => Promise.resolve())
-      .mockImplementationOnce(() => Promise.resolve())
-      .mockImplementationOnce(() => Promise.resolve());
+      const { closeConnection } = await import("./communication-service");
 
-    const { broadcast } = await import("./communication-service");
+      // Action / Assert
+      expect(
+        async () => await closeConnection(undefined as unknown as string)
+      ).not.toThrow();
 
-    // Action
-    await broadcast(connections, payload);
+      expect(sendFn.mock.calls.length).toBe(0);
+    });
 
-    expect(sendFn.mock.calls.length).toBe(2);
-    expect(
-      (sendFn.mock.calls[0][0] as PostToConnectionCommand).input.ConnectionId
-    ).toBe(connections[0].connectionId);
-    expect(
-      (sendFn.mock.calls[1][0] as PostToConnectionCommand).input.ConnectionId
-    ).toBe(connections[2].connectionId);
-  });
+    it("connection id is set, but client throws an error, error is captured and no error is returned", async () => {
+      // Arrange
+      const connectionId = "conn123";
 
-  it("payload is undefined, does not process any connections", async () => {
-    // Arrange
-    const connections: RoomConnection[] = [
-      { connectionId: "conn123" } as RoomConnection,
-      { connectionId: "conn456" } as RoomConnection,
-      { connectionId: "conn789" } as RoomConnection,
-    ];
+      sendFn.mockImplementationOnce(() => Promise.reject());
 
-    sendFn
-      .mockImplementationOnce(() => Promise.resolve())
-      .mockImplementationOnce(() => Promise.resolve())
-      .mockImplementationOnce(() => Promise.resolve());
+      const { closeConnection } = await import("./communication-service");
 
-    const { broadcast } = await import("./communication-service");
+      // Action / Assert
+      expect(async () => await closeConnection(connectionId)).not.toThrow();
 
-    // Action
-    await broadcast(connections, undefined);
+      expect(sendFn.mock.calls.length).toBe(1);
+      expect(
+        ((sendFn.mock.calls[0][0] as DeleteConnectionCommand).input as any)
+          .ConnectionId
+      ).toBe(connectionId);
+    });
 
-    // Assert
-    expect(sendFn.mock.calls.length).toBe(0);
+    it("connection id is set, and client does not throw an error, call is executed succcessfully", async () => {
+      // Arrange
+      const connectionId = "conn123";
+
+      sendFn.mockImplementationOnce(() => Promise.reject());
+
+      const { closeConnection } = await import("./communication-service");
+
+      // Action / Assert
+      expect(async () => await closeConnection(connectionId)).not.toThrow();
+
+      expect(sendFn.mock.calls.length).toBe(1);
+      expect(
+        ((sendFn.mock.calls[0][0] as DeleteConnectionCommand).input as any)
+          .ConnectionId
+      ).toBe(connectionId);
+    });
   });
 });
