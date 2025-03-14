b2b572f46e81768a42d9484a772229b7badb2afa
darylduck <daryl@lyraddigital.com>
2025-03-14 22:07:35 +1100

    repository tests for ensuring a connection

---
+++ lambda/handlers/websocket/ensure-room-connection-handler/src/repositories/transact-writes/update-room-user-count.spec.ts
@@ -0,0 +1,128 @@
+import { Room } from "@oigamez/models";
+
+import { updateRoomUserCount } from "./update-room-user-count";
+
+jest.mock("@oigamez/configuration", () => {
+  return {
+    DYNAMO_TABLE_NAME: "RoomTable",
+  };
+});
+
+describe("updateRoomUserCount tests", () => {
+  it("Creates the correct TransactWriteItem when room is public and current number of users will still be below max users", () => {
+    // Arrange
+    const roomCode = "ABCD";
+    const isPublic = true;
+    const currentNumberOfUsers = 2;
+    const maxNumberOfUsers = 4;
+    const room = {
+      code: roomCode,
+      isPublic,
+      curNumOfUsers: currentNumberOfUsers,
+      maxNumOfUsers: maxNumberOfUsers,
+    } as Room;
+
+    // Action
+    const transactWriteItem = updateRoomUserCount(room);
+
+    // Assert
+    expect(transactWriteItem.Update).toBeDefined();
+    expect(transactWriteItem.Update!.TableName).toBe("RoomTable");
+    expect(transactWriteItem.Update!.Key!).toEqual({
+      PK: { S: `Room#${roomCode}` },
+      SK: { S: "#Metadata" },
+    });
+    expect(transactWriteItem.Update!.UpdateExpression).toBe(
+      "ADD #curNumOfUsers :curNumOfUsers SET #visibilityType = :visibilityType"
+    );
+    expect(transactWriteItem.Update!.ConditionExpression).toBe(
+      "attribute_exists(PK) AND attribute_exists(SK)"
+    );
+    expect(transactWriteItem.Update!.ExpressionAttributeNames).toEqual({
+      "#curNumOfUsers": "CurNumberOfUsers",
+      "#visibilityType": "VisibilityType",
+    });
+    expect(transactWriteItem.Update!.ExpressionAttributeValues).toEqual({
+      ":curNumOfUsers": { N: "1" },
+      ":visibilityType": { S: "visible" },
+    });
+  });
+
+  it("Creates the correct TransactWriteItem when room is public and current number will equal max users", () => {
+    // Arrange
+    const roomCode = "ABCD";
+    const isPublic = true;
+    const currentNumberOfUsers = 3;
+    const maxNumberOfUsers = 4;
+    const room = {
+      code: roomCode,
+      isPublic,
+      curNumOfUsers: currentNumberOfUsers,
+      maxNumOfUsers: maxNumberOfUsers,
+    } as Room;
+
+    // Action
+    const transactWriteItem = updateRoomUserCount(room);
+
+    // Assert
+    expect(transactWriteItem.Update).toBeDefined();
+    expect(transactWriteItem.Update!.TableName).toBe("RoomTable");
+    expect(transactWriteItem.Update!.Key!).toEqual({
+      PK: { S: `Room#${roomCode}` },
+      SK: { S: "#Metadata" },
+    });
+    expect(transactWriteItem.Update!.UpdateExpression).toBe(
+      "ADD #curNumOfUsers :curNumOfUsers SET #visibilityType = :visibilityType"
+    );
+    expect(transactWriteItem.Update!.ConditionExpression).toBe(
+      "attribute_exists(PK) AND attribute_exists(SK)"
+    );
+    expect(transactWriteItem.Update!.ExpressionAttributeNames).toEqual({
+      "#curNumOfUsers": "CurNumberOfUsers",
+      "#visibilityType": "VisibilityType",
+    });
+    expect(transactWriteItem.Update!.ExpressionAttributeValues).toEqual({
+      ":curNumOfUsers": { N: "1" },
+      ":visibilityType": { S: "hidden" },
+    });
+  });
+
+  it("Creates the correct TransactWriteItem when room is private", () => {
+    // Arrange
+    const roomCode = "ABCD";
+    const isPublic = false;
+    const currentNumberOfUsers = 3;
+    const maxNumberOfUsers = 4;
+    const room = {
+      code: roomCode,
+      isPublic,
+      curNumOfUsers: currentNumberOfUsers,
+      maxNumOfUsers: maxNumberOfUsers,
+    } as Room;
+
+    // Action
+    const transactWriteItem = updateRoomUserCount(room);
+
+    // Assert
+    expect(transactWriteItem.Update).toBeDefined();
+    expect(transactWriteItem.Update!.TableName).toBe("RoomTable");
+    expect(transactWriteItem.Update!.Key!).toEqual({
+      PK: { S: `Room#${roomCode}` },
+      SK: { S: "#Metadata" },
+    });
+    expect(transactWriteItem.Update!.UpdateExpression).toBe(
+      "ADD #curNumOfUsers :curNumOfUsers SET #visibilityType = :visibilityType"
+    );
+    expect(transactWriteItem.Update!.ConditionExpression).toBe(
+      "attribute_exists(PK) AND attribute_exists(SK)"
+    );
+    expect(transactWriteItem.Update!.ExpressionAttributeNames).toEqual({
+      "#curNumOfUsers": "CurNumberOfUsers",
+      "#visibilityType": "VisibilityType",
+    });
+    expect(transactWriteItem.Update!.ExpressionAttributeValues).toEqual({
+      ":curNumOfUsers": { N: "1" },
+      ":visibilityType": { S: "hidden" },
+    });
+  });
+});
