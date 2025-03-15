02462d432843ae2accfc77bc9c598008ab524f35
darylduck <daryl@lyraddigital.com>
2025-03-15 17:23:46 +1100

    validate environment tests for external event sub subscribers

---
+++ lambda/handlers/subscribers/external/game-initialized-subscriber/src/configuration/validate-environment.spec.ts
@@ -0,0 +1,21 @@
+import {
+  verifyDynamoConnectionTableName,
+  verifyDynamoTableName,
+  verifyRoomSocketApiEndpoint,
+} from "@oigamez/configuration";
+
+jest.mock("@oigamez/configuration");
+
+import { validateEnvironment } from "./validate-environment";
+
+describe("validateEnvironment for external game initialized subscriptions", () => {
+  it("correct verify mocks were called", () => {
+    // Arrange / Action
+    validateEnvironment();
+
+    // Assert
+    expect(verifyDynamoConnectionTableName).toHaveBeenCalled();
+    expect(verifyDynamoTableName).toHaveBeenCalled();
+    expect(verifyRoomSocketApiEndpoint).toHaveBeenCalled();
+  });
+});
