6ba7e384d582c7927efc3f59ec4e21600f791659
darylduck <daryl@lyraddigital.com>
2025-03-16 16:20:52 +1100

    More tests

--- lambda/handlers/cron/expired-connections-handler/src/services/index.ts
+++ lambda/handlers/cron/expired-connections-handler/src/services/index.ts
@@ -1,3 +1,3 @@
-export * from "./get-all-rooms-from-connections";
-export * from "./publish-all-host-expirations";
-export * from "./publish-all-user-expirations";
+export * from "./get-all-rooms-from-connections.service";
+export * from "./publish-all-host-expirations.service";
+export * from "./publish-all-user-expirations.service";
