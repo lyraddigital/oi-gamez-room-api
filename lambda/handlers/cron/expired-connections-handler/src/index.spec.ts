e274eed356196d4f84d4123cf5803f9f3aa6158f
alinta-darylduck <Daryl.Duckmanton@alintaenergy.com.au>
2025-03-17 14:32:48 +1100

    Final tests for the expiry lambda

---
+++ lambda/handlers/cron/expired-connections-handler/src/index.spec.ts
@@ -0,0 +1,138 @@
+import { EventBridgeInternalEventType } from "@oigamez/event-bridge";
+import { Room, RoomConnection } from "@oigamez/models";
+import { convertFromMillisecondsToSeconds } from "@oigamez/services";
+import { EventBridgeEvent } from "aws-lambda";
+
+import { getAllExpiredConnections } from "./repositories";
+import {
+  getAllHostedRoomsFromConnections,
+  publishAllHostExpirations,
+  publishAllUserExpirations,
+} from "./services";
+import { handler } from ".";
+
+jest.mock("@oigamez/services");
+jest.mock("./configuration");
+jest.mock("./repositories");
+jest.mock("./services");
+
+describe("expired connections handler tests", () => {
+  it("no connections have expired, makes the correct calls", async () => {
+    // Arrange
+    const connections = [] as RoomConnection[];
+    const hostedRooms = [] as Room[];
+    const currentTimeInSeconds = 3933938;
+    const event = {} as EventBridgeEvent<
+      EventBridgeInternalEventType.expiredConnections,
+      void
+    >;
+
+    (
+      convertFromMillisecondsToSeconds as jest.MockedFunction<
+        typeof convertFromMillisecondsToSeconds
+      >
+    ).mockReturnValueOnce(currentTimeInSeconds);
+    (
+      getAllExpiredConnections as jest.MockedFunction<
+        typeof getAllExpiredConnections
+      >
+    ).mockResolvedValueOnce(connections);
+    (
+      getAllHostedRoomsFromConnections as jest.MockedFunction<
+        typeof getAllHostedRoomsFromConnections
+      >
+    ).mockResolvedValueOnce(hostedRooms);
+
+    // Action
+    await handler(event);
+
+    // Assert
+    expect(publishAllHostExpirations).not.toHaveBeenCalled();
+    expect(publishAllUserExpirations).not.toHaveBeenCalled();
+    expect(convertFromMillisecondsToSeconds).toHaveBeenCalled();
+    expect(getAllExpiredConnections).toHaveBeenCalledWith(currentTimeInSeconds);
+    expect(getAllHostedRoomsFromConnections).toHaveBeenCalledWith(connections);
+  });
+
+  it("2 connections are from hosts and 2 are from joiners, makes the correct calls", async () => {
+    // Arrange
+    const roomOneCode = "ABCD";
+    const roomTwoCode = "EFGH";
+    const roomThreeCode = "IJKL";
+    const roomFourCode = "MNOP";
+    const userOneUsername = "daryl_duck";
+    const userTwoUsername = "daryl_duck2";
+    const userThreeUsername = "daryl_duck3";
+    const userFourUsername = "daryl_duck4";
+    const roomOne = {
+      code: roomOneCode,
+      hostUsername: userOneUsername,
+      curNumOfUsers: 1,
+    } as Room;
+    const roomTwo = {
+      code: roomTwoCode,
+      hostUsername: userTwoUsername,
+      curNumOfUsers: 1,
+    } as Room;
+    const connectionOne = {
+      roomCode: roomOneCode,
+      username: userOneUsername,
+    } as RoomConnection;
+    const connectionTwo = {
+      roomCode: roomTwoCode,
+      username: userTwoUsername,
+    } as RoomConnection;
+    const connectionThree = {
+      roomCode: roomThreeCode,
+      username: userThreeUsername,
+    } as RoomConnection;
+    const connectionFour = {
+      roomCode: roomFourCode,
+      username: userFourUsername,
+    } as RoomConnection;
+    const connections = [
+      connectionOne,
+      connectionTwo,
+      connectionThree,
+      connectionFour,
+    ] as RoomConnection[];
+    const hostedRooms = [roomOne, roomTwo] as Room[];
+    const currentTimeInSeconds = 3933938;
+    const event = {} as EventBridgeEvent<
+      EventBridgeInternalEventType.expiredConnections,
+      void
+    >;
+
+    (
+      convertFromMillisecondsToSeconds as jest.MockedFunction<
+        typeof convertFromMillisecondsToSeconds
+      >
+    ).mockReturnValueOnce(currentTimeInSeconds);
+    (
+      getAllExpiredConnections as jest.MockedFunction<
+        typeof getAllExpiredConnections
+      >
+    ).mockResolvedValueOnce(connections);
+    (
+      getAllHostedRoomsFromConnections as jest.MockedFunction<
+        typeof getAllHostedRoomsFromConnections
+      >
+    ).mockResolvedValueOnce(hostedRooms);
+
+    // Action
+    await handler(event);
+
+    // Assert
+    expect(publishAllHostExpirations).toHaveBeenCalledWith([
+      { code: roomOneCode, curNumOfUsers: 0, hostUsername: userOneUsername },
+      { code: roomTwoCode, curNumOfUsers: 0, hostUsername: userTwoUsername },
+    ]);
+    expect(publishAllUserExpirations).toHaveBeenCalledWith([
+      { roomCode: roomThreeCode, username: userThreeUsername },
+      { roomCode: roomFourCode, username: userFourUsername },
+    ]);
+    expect(convertFromMillisecondsToSeconds).toHaveBeenCalled();
+    expect(getAllExpiredConnections).toHaveBeenCalledWith(currentTimeInSeconds);
+    expect(getAllHostedRoomsFromConnections).toHaveBeenCalledWith(connections);
+  });
+});
--- lambda/handlers/cron/expired-connections-handler/src/index.ts
+++ lambda/handlers/cron/expired-connections-handler/src/index.ts
@@ -5,8 +5,8 @@ import { convertFromMillisecondsToSeconds } from "@oigamez/services";
 
 import { validateEnvironment } from "./configuration";
 import { getAllExpiredConnections } from "./repositories";
-import { getAllHostedRoomsFromConnections } from "./services";
 import {
+  getAllHostedRoomsFromConnections,
   publishAllHostExpirations,
   publishAllUserExpirations,
 } from "./services";
@@ -16,31 +16,27 @@ validateEnvironment();
 export const handler = async (
   _: EventBridgeEvent<EventBridgeInternalEventType.expiredConnections, void>
 ): Promise<void> => {
-  try {
-    const currentTimeInSeconds = convertFromMillisecondsToSeconds(
-      Math.floor(new Date().getTime())
-    );
-    const connections = await getAllExpiredConnections(currentTimeInSeconds);
-    const hostedRooms = await getAllHostedRoomsFromConnections(connections);
-    const userOnlyConnections = connections.filter(
-      (c) => !hostedRooms.find((r) => r.hostUsername === c.username)
-    );
+  const currentTimeInSeconds = convertFromMillisecondsToSeconds(
+    Math.floor(new Date().getTime())
+  );
+  const connections = await getAllExpiredConnections(currentTimeInSeconds);
+  const hostedRooms = await getAllHostedRoomsFromConnections(connections);
+  const userOnlyConnections = connections.filter(
+    (c) => !hostedRooms.find((r) => r.hostUsername === c.username)
+  );
 
-    // We need to reduce the current number of users from the hosted rooms
-    // So that we can propertly calculate if the room is to be deleted or not
-    hostedRooms.forEach((hr) => {
-      const userCount = connections.filter((c) => c.roomCode == hr.code).length;
-      hr.curNumOfUsers -= userCount;
-    });
+  // We need to reduce the current number of users from the hosted rooms
+  // So that we can propertly calculate if the room is to be deleted or not
+  hostedRooms.forEach((hr) => {
+    const userCount = connections.filter((c) => c.roomCode == hr.code).length;
+    hr.curNumOfUsers -= userCount;
+  });
 
-    if (hostedRooms.length > 0) {
-      await publishAllHostExpirations(hostedRooms);
-    }
+  if (hostedRooms.length > 0) {
+    await publishAllHostExpirations(hostedRooms);
+  }
 
-    if (userOnlyConnections.length > 0) {
-      await publishAllUserExpirations(userOnlyConnections);
-    }
-  } catch (e) {
-    console.log(e);
+  if (userOnlyConnections.length > 0) {
+    await publishAllUserExpirations(userOnlyConnections);
   }
 };
